4e7ca7702c8f1534da41c6c238f8bdfb92a744a6
Logan Ward <ward.logan.t@gmail.com>
2023-12-26 18:10:01 -0500

    Test the statistics endpoint

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -10,3 +10,10 @@ def test_read_main():
     assert response.status_code == 200
     result = response.json()
     assert result['image_count'] >= 64  # The test file has 64 entries, a subset of what I have in my library
+
+
+def test_stats():
+    response = client.get("/statistics")
+    assert response.status_code == 200
+    result = response.json()
+    assert result['total_images'] >= 64  # The test file has 64 entries, a subset of what I have in my library
